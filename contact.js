3e9e68929bc82256c18d38f9f4faf2f1d126024c
kyledwagner <kylewag@me.com>
2019-10-15 17:01:02 +0700

    Moved the contact.php file to asia-reps as php mail server is set up there.

--- contact.js
+++ contact.js
@@ -12,7 +12,7 @@ $(function () {
 
         // if the validator does not prevent form submit
         if (!e.isDefaultPrevented()) {
-            var url = "http://eightatwork.com/contact.php";
+            var url = "http://next.asia-reps.com/contact.php";
 
             // POST values in the background the the script URL
             $.ajax({
